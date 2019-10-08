b80adc7c6acf2df9f50745f068e9d048ac7abbfe
cfigueirasvalverde <cfigueirasvalverde@nautilus8.ad.danielcastelao.org>
2019-10-08 08:59:07 +0200

    boletin1_3

---
+++ Boletin1_3/src/boletin1_3/Boletin1_3.java
@@ -0,0 +1,16 @@
+package boletin1_3;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin1_3 {
+
+    
+    public static void main(String[] args) {
+               float euro = 100, valorDolar = 1.16f;
+        System.out.println(euro+" euros son "+euro*valorDolar+" dolares");
+        
+    }
+    
+}
